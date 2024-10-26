0683f25632eae4ac6bec012659a410db632876c0
zimirken <rumyan777777@gmail.com>
2024-10-26 13:58:30 +0300

    maint: refactor code

--- backend/server.ts
+++ backend/server.ts
@@ -1,22 +1,24 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import { PrismaClient } from '@prisma/client'
 
 dotenv.config()
 
 const app = express()
+app.use(cors())
+
 const prisma = new PrismaClient()
 
 const main = async () => {
+  await prisma.$connect()
   app.listen(process.env.PORT, () =>
     console.log(`Listening on port ${process.env.PORT}`)
   )
 }
 
-main().then(async () => {
-  await prisma.$connect().catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+main().catch(async (e) => {
+  console.error(e)
+  await prisma.$disconnect()
+  process.exit(1)
 })
