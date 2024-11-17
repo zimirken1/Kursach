d3e12e7db4634608447a89151e1018168a65d728
zimirken1 <rumyan1337@gmail.com>
2024-11-17 21:10:41 +0300

    fix: errors middleware

--- backend/src/middleware/error-middleware.ts
+++ backend/src/middleware/error-middleware.ts
@@ -1,17 +1,21 @@
-import { ErrorRequestHandler, Request, Response } from 'express';
-import { ApiError } from '../exceptions/api-error';
-import { Status } from '../exceptions/api-error';
+import { ErrorRequestHandler } from 'express';
+import { ApiError, Status } from '../exceptions/api-error';
 
-export const errorMiddleware: ErrorRequestHandler = (error: Error, req: Request, res: Response) => {
+export const errorMiddleware: ErrorRequestHandler = (error, req, res, next) => {
+  if (res.headersSent) {
+    return next(error);
+  }
+
   if (error instanceof ApiError) {
     res.status(error.status).json({
       status: error.status,
       message: error.message,
-      errors: error.errors,
+      errors: error.errors || null,
     });
+    return;
   }
 
-  console.log(error);
+  console.error('Ошибка:', error);
 
   res.status(Status.INTERNAL_SERVER_ERROR).json({
     status: Status.INTERNAL_SERVER_ERROR,
